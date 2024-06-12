7eeb81c04728441f7da7a80fcb3e2fe18c9c91a5
DimaOshchepkov <odo.2003@yandex.ru>
2024-06-13 01:39:18 +0300

    Закончил

--- Benchmark/BM_pagerank.cpp
+++ Benchmark/BM_pagerank.cpp
@@ -10,16 +10,18 @@
 
 using namespace std;
 string solutionDir;
-string getSolutionDir(std::string short_path) {
+string getSolutionDir(std::string short_path)
+{
     if (!solutionDir.empty())
         return solutionDir + "\\" + short_path;
     string s = EXPAND(SOLDIR);
-    s.erase(0, 1); // erase the first quote
+    s.erase(0, 1);         // erase the first quote
     s.erase(s.size() - 2); // erase the last quote and the dot
 
     size_t found = s.find_last_of('\\');
 
-    if (found != std::string::npos) {
+    if (found != std::string::npos)
+    {
         s = s.substr(0, found);
     }
 
@@ -27,72 +29,79 @@ string getSolutionDir(std::string short_path) {
     return solutionDir + "\\" + short_path;
 }
 
-static void BM_build_transition(benchmark::State& state) {
+static void BM_build_transition(benchmark::State &state)
+{
     using namespace ranking;
-    
+
     auto path = getSolutionDir(R"(res\rating.csv)");
-    for (auto _ : state) {
+    for (auto _ : state)
+    {
         auto x = TransitionMatrixBuilder()
-            .SetPath(path)
-            .SetSourceName("userId")
-            .SetTargetName("movieId")
-            .SetEdgeAttributeName("rating")
-            .Build();
+                     .SetPath(path)
+                     .SetSourceName("userId")
+                     .SetTargetName("movieId")
+                     .SetEdgeAttributeName("rating")
+                     .Build();
         benchmark::DoNotOptimize(x);
     }
 }
 
-static void BM_Pagårank_MovieLens(benchmark::State& state, double sample) {
+static void BM_Pagerank_MovieLens(benchmark::State &state, double sample)
+{
     using namespace ranking;
     auto path = getSolutionDir(R"(res\rating.csv)");
     auto [M, map] = TransitionMatrixBuilder()
-        .SetPath(path)
-        .SetSourceName("userId")
-        .SetTargetName("movieId")
-        .SetEdgeAttributeName("rating")
-        .SetSample(sample)
-        .Build();
+                        .SetPath(path)
+                        .SetSourceName("userId")
+                        .SetTargetName("movieId")
+                        .SetEdgeAttributeName("rating")
+                        .SetSample(sample)
+                        .Build();
 
     int row_index = 1;
     auto dense_vec = get_personalization_from_matrix_row(M, row_index);
 
-    for (auto _ : state) {
+    for (auto _ : state)
+    {
         auto pagerank = PageRankFactory()
-            .SetTransitionMatrix(M)
-            .SetPersonalizationVector(dense_vec)
-            .Build();
+                            .SetTransitionMatrix(M)
+                            .SetPersonalizationVector(dense_vec)
+                            .Build();
         benchmark::DoNotOptimize(pagerank);
     }
 }
 
-static void BM_Hits_MovieLens(benchmark::State& state, double sample) {
+static void BM_Hits_MovieLens(benchmark::State &state, double sample)
+{
     using namespace ranking;
     auto path = getSolutionDir(R"(res\rating.csv)");
     auto [M, map] = TransitionMatrixBuilder()
-        .SetPath(path)
-        .SetSourceName("userId")
-        .SetTargetName("movieId")
-        .SetEdgeAttributeName("rating")
-        .SetSample(sample)
-        .Build();
+                        .SetPath(path)
+                        .SetSourceName("userId")
+                        .SetTargetName("movieId")
+                        .SetEdgeAttributeName("rating")
+                        .SetSample(sample)
+                        .Build();
 
     int row_index = 1;
     auto dense_vec = get_personalization_from_matrix_row(M, row_index);
 
-    for (auto _ : state) {
+    for (auto _ : state)
+    {
         auto pagerank = HITSFactory()
-            .SetTransitionMatrix(M)
-            .Build();
+                            .SetTransitionMatrix(M)
+                            .Build();
         benchmark::DoNotOptimize(pagerank);
     }
 }
 
-
-int main(int argc, char** argv) {
-    for (double sample = 0.1; sample <= 1.01; sample += 0.1) {
+int main(int argc, char **argv)
+{
+    for (double sample = 0.1; sample <= 1.01; sample += 0.1)
+    {
         benchmark::RegisterBenchmark(
             std::format("BM_Pagerank_MovieLens/sample-{:.2f}", sample),
-            BM_Pagårank_MovieLens, sample)
+            BM_Pagerank_MovieLens, sample)
             ->Iterations(3)
             ->Unit(benchmark::kSecond);
 
@@ -101,7 +110,6 @@ int main(int argc, char** argv) {
             BM_Hits_MovieLens, sample)
             ->Iterations(3)
             ->Unit(benchmark::kSecond);
-
     }
 
     benchmark::Initialize(&argc, argv);
---
+++ README.md
@@ -0,0 +1,48 @@
+# Кусовой проект на тему "Разработка системы ссылочного ранжирования"
+
+Этот курсовой проект был создан в рамках дисциплины "Терория алгоритмов". В нем рассматриваются 2 наиболее распространенных алгоритма ссылочного ранжирования: Pagrank и HITS.
+
+## Быстрый старт
+Пример использования pagrank :
+```cpp
+auto x = TransitionMatrixBuilder()
+            .SetPath("path/to/datafram")
+            .SetSourceName("userId")
+            .SetTargetName("movieId")
+            .SetEdgeAttributeName("rating")
+            .Build();
+
+int row_index = 1;
+auto dense_vec = get_personalization_from_matrix_row(M,row_index);
+
+
+auto pagerank = PageRankFactory()
+    .SetTransitionMatrix(M)
+    .SetPersonalizationVector(dense_vec)
+    .Build();
+```
+
+pagerank - будет вектором персонализации.
+
+Пример использования HITS:
+```cpp
+
+auto x = TransitionMatrixBuilder()
+            .SetPath("path/to/datafram")
+            .SetSourceName("userId")
+            .SetTargetName("movieId")
+            .SetEdgeAttributeName("rating")
+            .Build();
+
+int row_index = 1;
+auto dense_vec = get_personalization_from_matrix_row(M,row_index);
+
+
+auto [auth, hubs] = HITSFactory()
+    .SetTransitionMatrix(M)
+    .SetPersonalizationVector(dense_vec)
+    .Build();
+
+```
+
+Этот проект является вспомогательным (здесь только замеры времени). Основное исследование [тут](https://github.com/DimaOshchepkov/PageRank-Course-Project/blob/main/plot_comparision.ipynb).
